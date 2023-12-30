632740fc52248ca156c7662be723a2408a1c861d
Edouard Chevenslove <edouardchevenslove98@gmail.com>
2023-12-30 16:09:16 +0100

    Grouper les messages par date

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:m2dfs_chat_app/constants.dart';
 import 'package:m2dfs_chat_app/viewmodel/auth_viewmodel.dart';
 import 'package:m2dfs_chat_app/widgets/loading_screen.dart';
 import 'package:m2dfs_chat_app/widgets/new_message_section.dart';
 import 'package:provider/provider.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:collection/collection.dart';
 
 import '../model/Message.dart';
 import '../viewmodel/chat_view_model.dart';
@@ -32,6 +35,7 @@ class _ChatPageState extends State<ChatPage> {
   String? chatId;
   late String senderId;
 
+
   @override
   void initState() {
     super.initState();
@@ -45,6 +49,7 @@ class _ChatPageState extends State<ChatPage> {
         chatId = id;
       });
     });
+
   }
 
   @override
@@ -66,35 +71,66 @@ class _ChatPageState extends State<ChatPage> {
       body: Column(
         children: [
           Expanded(
-          child: StreamBuilder<List<Message>>(
-          stream: chatViewModel.getTwoUserChatMessages(chatId!),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const LoadingScreen();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              final messages = snapshot.data ?? [];
-              return ListView.separated(
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: Insets.xs,
-                ),
-                reverse: true,
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  final message = messages[index];
-                  return MessageItem(
-                    text: message.content,
-                    senderId: message.from,
-                    date: DateTime.fromMillisecondsSinceEpoch(int.parse(message.timestamp)),
-                    isSent: message.from == senderId,
+            child: StreamBuilder<List<Message>>(
+              stream: chatViewModel.getTwoUserChatMessages(chatId!),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const LoadingScreen();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  final messages = snapshot.data ?? [];
+
+                  final groupedMessages = groupBy(messages, (Message message) {
+                    return DateFormat('dd/MM/yyyy').format(DateTime.fromMillisecondsSinceEpoch(int.parse(message.timestamp)));
+                  });
+
+                  return GroupedListView<dynamic, String>(
+                    elements: groupedMessages.entries.toList(),
+                    groupBy: (element) => element.key,
+                    groupSeparatorBuilder: (String value) {
+                      return Center(
+                        child: Container(
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Text(
+                            value,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    itemBuilder: (context, messagesGrouped) {
+                      final List<Message> sortedMessages = messagesGrouped.value
+                        ..sort((Message a, Message b) => a.timestamp.compareTo(b.timestamp));
+
+                      final List<MessageItem> messageItems = [];
+
+                      for (var message in sortedMessages) {
+                        final messageItem = MessageItem(
+                          text: message.content,
+                          senderId: message.from,
+                          date: DateTime.fromMillisecondsSinceEpoch(int.parse(message.timestamp)),
+                          isSent: message.from == senderId,
+                        );
+
+                        messageItems.add(messageItem);
+                      }
+
+                      return Column(
+                        children: messageItems,
+                      );
+                    },
                   );
-                },
-              );
-            }
-          },
-        ),
-        ),
+                }
+              },
+            ),
+          ),
           NewMessageSection(
             chatId: chatId!,
             receiverId: widget.receiverId,
